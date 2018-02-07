8294916f76ee5e9e786bdf7ce6ddb0b4aa0309d7
Logan Turske <Logan.Turske@ngc.com>
2018-02-07 13:59:19 -0500

    Commented

--- main.py
+++ main.py
@@ -110,31 +110,40 @@ def stdev(arr):
 	# Get the standard deviation of the entire array that was passed in
 	# Start by getting the variance
 	variance = sum([pow(x-mean(arr),2) for x in arr])/float(len(arr)-1)
+	print variance
 	# Then take the square root of the variance
 	return math.sqrt(variance)
 
 def summarize(data):
 	# the "zip" function will give us an iterable for each row of the data so we can get the attributes
+	#	for evey instance of data
 	summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*data)]
+	# Remove the summaries for the first attribute which is the "answer"
 	del summaries[0]
 	return summaries
 
 def summarize_by_class(data):
+	# Separate all of the classes
 	separated = separate_by_class(data)
 	summaries = {}
+	# Get the summaries for each of the separated classes
 	for classValue, instances in separated.iteritems():
 		summaries[classValue] = summarize(instances)
 	return summaries
 
 def calculate_probability(x, mean, stdev):
+	if (2*math.pow(stdev,2)) == 0:
+		return 1
 	# Gaussian Probablility
 	exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
 	return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
 
 def calculate_class_probabilities(summaries, inputVector):
+	# Get the probabilities of all of the summaries
 	probabilities = {}
 	for classValue, classSummaries in summaries.iteritems():
 		probabilities[classValue] = 1
+		# For each class summary calculate the probability and multiply it with the others
 		for i in range(len(classSummaries)):
 			mean, stdev = classSummaries[i]
 			x = inputVector[i]
@@ -142,8 +151,10 @@ def calculate_class_probabilities(summaries, inputVector):
 	return probabilities
 
 def predict(summaries, inputVector):
+	# Get the probability of all the classes
 	probabilities = calculate_class_probabilities(summaries, inputVector)
 	bestLabel, bestProb = None, -1
+	# For each of the probabilies get the the best
 	for classValue, probability in probabilities.iteritems():
 		if bestLabel is None or probability > bestProb:
 			bestProb = probability
@@ -151,6 +162,7 @@ def predict(summaries, inputVector):
 	return bestLabel
 
 def get_predictions(summaries, testSet):
+	# Go through the test set and make predictions
 	predictions = []
 	for i in range(len(testSet)):
 		result = predict(summaries, testSet[i])
@@ -158,6 +170,7 @@ def get_predictions(summaries, testSet):
 	return predictions
 
 def get_accuracy(testSet, predictions):
+	# Calculate Accuracy
 	correct = 0
 	for x in range(len(testSet)):
 		if testSet[x][0] == predictions[x]:
@@ -165,15 +178,31 @@ def get_accuracy(testSet, predictions):
 	return (correct/float(len(testSet))) * 100.0
 
 def naive_bayes_algo(test_set, training_set):
+	# Summarize the attributes
 	summaries = summarize_by_class(training_set)
 	print summaries
+	# Get the predictions of the test set
 	predictions = get_predictions(summaries, test_set)
+	#Get the accuracy
 	accuracy = get_accuracy(test_set, predictions)
 	print('Naive Bayes Accuracy: {0}%').format(accuracy)
 
 def main():
 
+	print "############ breast-cancer-wisconsin.data ############"
 	data = load_CSV('datasets/breast-cancer-wisconsin.data')
+	test_set, training_set = split_data(data)
+	winnow_algo(test_set, training_set, 2, .5)
+	naive_bayes_algo(test_set, training_set)
+
+	print "############ house-votes-84.data ############"
+	data = load_CSV('datasets/house-votes-84.data')
+	test_set, training_set = split_data(data)
+	winnow_algo(test_set, training_set, 2, .5)
+	naive_bayes_algo(test_set, training_set)
+
+	print "############ iris.data ############"
+	data = load_CSV('datasets/iris.data')
 	test_set, training_set = split_data(data)
 	winnow_algo(test_set, training_set, 2, .5)
 	naive_bayes_algo(test_set, training_set)
